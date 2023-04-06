d57dd2446a3f5a4f60984356244251febdb38cc5
Hemanth Krishna <hkpdev008@gmail.com>
2023-04-06 18:33:51 +0530

    feat: deployment and traefik routing
    Signed-off-by: Hemanth Krishna <hkpdev008@gmail.com>

--- run.sh
+++ run.sh
@@ -1,19 +1,19 @@
 echo "Cloning Sorobix-Formatter"
 git clone https://github.com/sorobix/sorobix-formatter
 echo "Cloning Sorobix-API"
-git clone https://github.com/sorobix/sorobix-api
+git clone https://github.com/sorobix/sorobix-api-rs
 echo "Entering formatter"
 cd sorobix-formatter
 echo "Fetching latest code"
 git pull origin master
 echo "Building sorobix-formatter"
-docker build . -t sorobix-formatter
+docker build . -t sorobix-formatter:latest
 echo "Entering api"
-cd ../sorobix-api
+cd ../sorobix-api-rs
 echo "Fetching latest code"
-git pull origin main
-echo "Building sorobix-api"
-docker build . -t sorobix-api
+git pull origin master
+echo "Building sorobix-api-rs"
+docker build . -t sorobix-api-rs:latest
 cd ..
 echo "Turning up the service"
 docker-compose up --build --detach
