f271eab60963e3cc504d7dae54fd974ca4edbc06
Hemanth Krishna <hkpdev008@gmail.com>
2023-06-07 19:32:32 +0530

    feat: change from formatter to wasm-handler
    Signed-off-by: Hemanth Krishna <hkpdev008@gmail.com>

--- run.sh
+++ run.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-echo "Cloning Sorobix-Formatter"
-git clone https://github.com/sorobix/sorobix-formatter
+echo "Cloning Sorobix-Wasm-Handler"
+git clone https://github.com/sorobix/wasm-handler
 echo "Cloning Sorobix-API"
 git clone https://github.com/sorobix/sorobix-api-rs
-echo "Entering formatter"
-cd sorobix-formatter
+echo "Entering wasm handler"
+cd wasm-handler
 echo "Fetching latest code"
-git pull origin master
-echo "Building sorobix-formatter"
-docker build . -t sorobix-formatter:latest
+git pull origin main
+echo "Building wasm-handler"
+docker build . -t wasm-handler:latest
 echo "Entering api"
 cd ../sorobix-api-rs
 echo "Fetching latest code"
